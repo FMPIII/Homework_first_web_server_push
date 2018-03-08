4e631aa02326edf3f68512e0f79df30c545a36e2
Frank Patterson <twoandapossible@hotmail.com>
2018-03-08 16:15:55 -0500

    first web server push

---
+++ README.md
@@ -0,0 +1,3 @@
+This is some homework we had pushing a file we created at wyncode
+to GitHub.  We used atom and ruby to make this simple file.
+Thanks for not laughing!
--- server.rb
+++ server.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
 
-get '/' do         #a method. Get accepts a string and a block. this string is a url path '/' the block do end must have a string in side of it.
-  "Hello world"
+get '/' do
+  File.open("public/hello.txt")
 end
 
-get "/sinatra" do
-  "Hello Sinatra"
-end
+
+# get '/' do         #a method. Get accepts a string and a block. this string is a url path '/' the block do end must have a string in side of it.
+#   "Hello world"
+# end
+#
+# get "/sinatra" do
+#   "Hello Sinatra"
+# end
